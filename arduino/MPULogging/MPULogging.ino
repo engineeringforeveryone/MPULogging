59e77a48809a6662e60c96a140ae20dcd816220e
Dan B <dbusan@users.noreply.github.com>
2018-09-04 21:03:15 +0800

    Initial commit

---
+++ arduino/MPULogging/MPULogging.ino
@@ -0,0 +1,180 @@
+// MPULogging.ino
+// This sketch is based on TechMonkeyBusiness's GY87 reader test.
+// 
+// // ORIGINAL COMMENTS
+// It gets all sensors on a GY87 IMU board reporting (something) to the
+// Serial.  The challenge is the Digital Compass which is nromally blocked by
+// the Accelerometer.  This is remedied by using an I2C bypass command.  Thanks to
+// pistolero992000 for this solution.
+// Sensors on board the GY87 are:
+// MPU6050 Accelerometer.  Address is 0x68
+// HMC5883L Digital Compass.  Address is 0x1E
+// BMP180 Barometer and Temperature Sensor.  Address is 0x77
+
+// I2C Connections as per device default
+// SCL and SDA
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
+
+// 500 Hz sampling rate
+#define SAMPLING_RATE 250
+
+//MPU6050 Accelerometer 
+MPU6050 accelgyro;
+
+int16_t ax, ay, az;
+int16_t gx, gy, gz;
+
+//HMC5883L Digital Compass
+const int hmc5883Address = 0x1E; //0011110b, I2C 7bit address for compass
+const byte hmc5883ModeRegister = 0x02;
+const byte hmcContinuousMode = 0x00;
+const byte hmcDataOutputXMSBAddress = 0x03;
+
+int x,y,z; //triple axis data from HMC5883L.
+
+
+float seaLevelPressure = 101325;
+
+int LEDPin = 13;
+bool blinkState = false;
+
+void setup()
+{
+    Wire.begin();
+    Serial.begin(9600);
+
+    // initialise I2C Devices
+    Serial.println("Initialising I2C devices...");
+    accelgyro.initialize();
+
+    // set accelerometer to 8g
+    accelgyro.setFullScaleAccelRange(MPU6050_ACCEL_FS_8);
+    // set gyro range to 1000 degrees per second
+    accelgyro.setFullScaleGyroRange(MPU6050_GYRO_FS_1000);
+
+    // verify connection
+    Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+    accelgyro.setI2CBypassEnabled(true);  //This sets the bypass so the HMC5883L gets a look in.
+
+    
+          
+    //Initialise the Digital Compass
+    Wire.beginTransmission(hmc5883Address);  //Begin communication with compass
+    Wire.write(hmc5883ModeRegister);  //select the mode register
+    Wire.write(hmcContinuousMode); //continuous measurement mode
+    Wire.endTransmission();
+    
+        
+    // configure Arduino LED for
+    pinMode(LEDPin, OUTPUT);
+    delay(1000);
+}
+
+// sampling rate 500 hz
+// mainly inspired from TechMonkeyBusiness's main loop, stripped down to get the data that we want.
+unsigned long int current_micros = 0;
+unsigned long int prev_sample_micros= 0;
+// get sampling rate in microseconds 
+const long int SAMPLING_PERIOD = 1000/SAMPLING_RATE * 1000;
+
+
+// timing test
+// microseconds has a resolution of 4 microseconds
+unsigned long int start_time = 0;
+unsigned long int end_time = 0;
+
+long long last_serial_ms = 0;
+
+void loop()
+{
+    // sample every SAMPLING_PERIOD microseconds
+    current_micros = micros();
+    if (current_micros - prev_sample_micros > SAMPLING_PERIOD)
+    {
+      start_time = micros();
+        accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+        
+        Wire.beginTransmission(hmc5883Address);
+        Wire.write(hmcDataOutputXMSBAddress);  //Select register 3, X MSB register
+        Wire.endTransmission();
+    
+        //Read data from each axis of the Digital Compass
+        Wire.requestFrom(hmc5883Address,6);
+        if(6<=Wire.available())
+        {
+            x = Wire.read()<<8; //X msb
+            x |= Wire.read();   //X lsb
+            z = Wire.read()<<8; //Z msb
+            z |= Wire.read();   //Z lsb
+            y = Wire.read()<<8; //Y msb
+            y |= Wire.read();   //Y lsb    
+        }
+        end_time = micros();
+        prev_sample_micros = current_micros;
+    }
+    // read raw accel/gyro measurements from device
+    
+
+
+    // blink LED to indicate activity
+    blinkState = !blinkState;
+    digitalWrite(LEDPin, blinkState);
+    
+  
+    //Accessing the HMC5883L Digital Compass  
+    //Tell the HMC5883L where to begin reading the data
+//    print_debug();
+
+    
+    // int angle = atan2(-y,x)/M_PI*180;
+    // if (angle < 0)
+    // {
+    //   angle = angle + 360;
+    // }
+    
+    // //Reporting the Compass data to the Serial port
+    // //Serial.print("Compass XYZ:\t");
+    // //Serial.print(x,y,z);Serial.print("\t");
+    // Serial.print("Dir(deg):\t");
+    // Serial.print(angle); Serial.print("\t");
+
+    // these methods (and a few others) are also available 
+    //accelgyro.getAcceleration(&ax, &ay, &az);
+    //accelgyro.getRotation(&gx, &gy, &gz);
+    
+    // // display tab-separated accel/gyro,compass x/y/z 
+     Serial.print(ax); Serial.print(",");
+     Serial.print(ay); Serial.print(",");
+     Serial.print(az); 
+    //  Serial.print(",");
+    //  Serial.print(gx); Serial.print(",");
+    //  Serial.print(gy); Serial.print(",");
+    //  Serial.print(gz); Serial.print(",");
+    //  Serial.print(x); Serial.print(",");
+    //  Serial.print(y); Serial.print(",");
+    //  Serial.print(z); 
+    Serial.print('\n');
+    // terminate transmission with a newline
+}
+
+
+void print_debug()
+{
+  if (millis() - last_serial_ms > 500)
+  {
+    last_serial_ms = millis();
+    Serial.print(start_time); Serial.print(","); Serial.print(end_time); 
+    Serial.print(",");  
+    int res = int(end_time - start_time);
+    Serial.print(res);
+    
+  //        Serial.print(end_time - start_time); 
+    Serial.print(" us.");
+    Serial.println();
+  }
+}
+
---
+++ py/main.py
@@ -0,0 +1,115 @@
+# serial.py
+
+import serial
+import keyboard
+
+import sys
+import glob
+import serial
+
+import threading
+import datetime
+
+# https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+# ============================================================================ #
+
+import os
+
+def write_file(data):
+    filename =  datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + '.csv'
+    filepath = 'out/' + filename
+
+    with open(filepath, 'w') as outfile:
+        
+        for line in data:
+            outfile.write(line)
+        
+    print('Successfully wrote to file: ' + filename)
+
+
+# data from Arduino is in the format
+# accelerometer, gyroscope, compass
+# ax, ay, az, gx, gy, gz, cx, cy, cz
+def main():
+    available_ports = serial_ports()
+
+    print(available_ports)
+
+    # connect to serial port
+    # for now serial port is COM5 or second port returned by serial_ports()
+    logger_port = 'COM10'
+    if len(available_ports) > 1:
+        logger_port = available_ports[1]
+    else:
+        exit()
+
+    
+
+    # set up serial
+    ser = serial.Serial()
+    ser.baudrate = 9600
+    ser.port = logger_port
+    ser.open()
+
+    
+    if ser.is_open:
+        print('\nLogging...')
+        
+        data_buffer = list()
+        count = 0
+        while True:
+                
+            in_line = ser.readline().decode('utf-8')
+            data_buffer.append(in_line)
+
+            # 64,15796,-4808,-300,21,-85,20,610,-48
+
+            count += 1
+            # quit if q is pressed
+            if keyboard.is_pressed('q'):
+                print('Quitting')
+                ser.close()
+                break
+
+            if count % 2500 == 0:
+                thread_writefile = threading.Thread(target=write_file, args=(data_buffer.copy(), ))
+                thread_writefile.start()
+                print('2500 lines read.')
+                data_buffer = []
+                
+                
+                
+
+        print(count, 'lines read.')
+
+if __name__=='__main__':
+    main()
